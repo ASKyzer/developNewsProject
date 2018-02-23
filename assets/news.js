d4d442014067c27eb516e9531a6ffe5f4656ad7d
unknown <marshquinn@aol.com>
2018-02-23 13:23:08 -0500

    js update 3

--- assets/news.js
+++ assets/news.js
@@ -1,12 +1,13 @@
 $("#submit").on("click", function() {
 
-    var search = $("#terms").val();
+    var search = $("#terms").val().trim();
     var limit = $("#limits").val();
-    var start = $("#startyear").val();
+    //var limit = 2;
+    var start = $("#startYear").val().trim();
     if (start == "") {
         start = "1852";
     }
-    var end = $("#endyear").val();
+    var end = $("#endYear").val().trim();
     if (end == "") {
         end = "2017";
     }
@@ -21,15 +22,33 @@ $("#submit").on("click", function() {
     'page': 0
     });
 
+    // var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    // queryURL += '?' + $.param({
+    //     'api-key': "61672c1c287440d4a8d53b4400b9a62e",
+    //     'q': "star wars",
+    //     'begin_date': "19730101",
+    //     'end_date': "20081231",
+    //     'sort': "newest",
+    //     'page': 0
+    // });
+    console.log(queryURL);
 
-    $.ajax ({
+    $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
+        var results = response.response.docs;
+        console.log(results);
         for (i=0; i<limit; i++) {
             var entry = $("<div>");
-            console.log(response);
-            
+            //console.log(response);
+            var headline = $("<h2>").text(results[i].headline.main);
+            var byline = $("<h3>").text(results[i].byline.original);
+            var publish = $("<h4>").text(results[i].pub_date);
+            var abstract = $("<p>").text(results[i].abstract);
+            var link = $("<a>").attr("href", results[i].web_url).text("Read More");
+            entry.append(headline, byline, publish, abstract, link);
+            $("#articles-goes-here").append(entry);
 
         }
     })
@@ -38,3 +57,6 @@ $("#submit").on("click", function() {
 //$("#clear").on("click", function() {
 
 //})
+
+
+
